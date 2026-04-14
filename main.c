a344ccb595301c9da9446dd878006b0ef95559be
MrRooby <mrruby64@gmail.com>
2026-04-14 21:15:28 +0200

    green led on when moisture low, red led on when battery low

--- main.c
+++ main.c
@@ -4,27 +4,40 @@
 #include "stm8l15x_gpio.h"
 #include <stdio.h>
 #include "serial.h"
-#include "stm8l15x_usart.h"
 
 #define WET_VAL 980
 #define DRY_VAL 2755
+
+#define RED_PORT GPIOA
+#define BLUE_PORT GPIOD
+#define DIODE_PORT GPIOB
+#define CAP_PORT GPIOA
+#define RED_PIN GPIO_Pin_3
+#define BLUE_PIN  GPIO_Pin_0
+#define DIODE_PIN GPIO_Pin_1
+#define CAP_PIN GPIO_Pin_0
 
 void setupADC(void);
 uint16_t readSensor(void);
 uint16_t readVREF(void);
 void readMoistureSensor(uint8_t enableSerial);
+void Delay(uint32_t tics);
 
 int main(void) {
-  GPIO_Init(GPIOA, GPIO_Pin_3, GPIO_Mode_Out_PP_High_Fast);
+  GPIO_Init(RED_PORT, RED_PIN, GPIO_Mode_Out_PP_High_Fast);
+  GPIO_Init(DIODE_PORT, DIODE_PIN, GPIO_Mode_Out_PP_High_Fast);
+  GPIO_Init(CAP_PORT, CAP_PIN, GPIO_Mode_Out_PP_High_Fast);
+  GPIO_Init(BLUE_PORT, BLUE_PIN, GPIO_Mode_Out_PP_High_Fast);
+  GPIO_SetBits(DIODE_PORT, DIODE_PIN);
+  GPIO_SetBits(CAP_PORT, CAP_PIN);
+  GPIO_SetBits(BLUE_PORT, BLUE_PIN);
 
   Serial_begin(115200);
   setupADC();
-  
+
   while(1) {
     readMoistureSensor(1);
-    for (volatile uint32_t i = 0; i < 2000000; i++);
-    while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
-    __asm__("halt");
+    Delay(800000);
   }
 }
 
@@ -45,7 +58,7 @@ void setupADC(void){
 
   ADC_Cmd(ADC1, ENABLE);
 
-  for (volatile uint32_t i = 0; i < 50000; i++);
+  Delay(50000);
 }
 
 uint16_t readSensor(void){
@@ -70,6 +83,10 @@ void readMoistureSensor(uint8_t enableSerial){
     uint16_t vref_raw = readVREF();
 
     uint32_t actual_vdd = (1225UL * 1024) / vref_raw;
+    // Low battery warning
+    if(actual_vdd < 2900) GPIO_SetBits(RED_PORT, RED_PIN);
+    else GPIO_ResetBits(RED_PORT, RED_PIN);
+    
     uint32_t sensor_mV = ((uint32_t)sensor_raw * actual_vdd)  / 1024;
 
     uint32_t moist_pctg = 0;
@@ -77,8 +94,18 @@ void readMoistureSensor(uint8_t enableSerial){
     else if (sensor_mV <= WET_VAL) moist_pctg = 100;    
     else moist_pctg = ((DRY_VAL - sensor_mV) * 100) / (DRY_VAL - WET_VAL);
 
-    if(moist_pctg < 10) GPIO_SetBits(GPIOA, GPIO_Pin_3);
-    else GPIO_ResetBits(GPIOA, GPIO_Pin_3);
+
+    if(moist_pctg < 10){
+      GPIO_SetBits(CAP_PORT, CAP_PIN); // Charging capacitor
+      GPIO_ResetBits(BLUE_PORT, BLUE_PIN); // Selecting LED
+      GPIO_ResetBits(DIODE_PORT, DIODE_PIN); // Discharging through diode
+      GPIO_SetBits(DIODE_PORT, DIODE_PIN); // Discharging through diode
+    } 
+    else{
+      GPIO_ResetBits(CAP_PORT, CAP_PIN); // Charging capacitor
+      GPIO_SetBits(BLUE_PORT, BLUE_PIN); // Selecting LED
+      GPIO_SetBits(DIODE_PORT, DIODE_PIN); // Discharging through diode
+    }
 
     if(enableSerial == 1){
       printf("\nsensor:   %d\n\r", sensor_raw);
@@ -88,4 +115,8 @@ void readMoistureSensor(uint8_t enableSerial){
       printf("Moisture: %lu%%\n\r", moist_pctg);
       printf("----------------");
     }
+}
+
+void Delay(uint32_t tics){
+    for (volatile uint32_t i = 0; i < tics; i++);
 }
